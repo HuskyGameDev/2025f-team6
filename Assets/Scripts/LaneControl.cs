f767c88f82cdd2bb52900ea78bd5a7b160444faf
primeortis <dajeepk@gmail.com>
2025-10-07 18:11:16 -0400

    Reworked player movement and added acceleration

--- Assets/Scripts/LaneControl.cs
+++ Assets/Scripts/LaneControl.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class LaneControl : MonoBehaviour
@@ -5,20 +6,61 @@ public class LaneControl : MonoBehaviour
     public int distance;
     public GameObject lane;
     static private int numLanes = 5;
-    static private int currentLanes = 0;
+    static private GameObject[] lanes;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        while (currentLanes < numLanes)
+        lanes = lane.GetComponents<GameObject>();
+    }
+
+    /**
+     * getLane 
+     * Input number of lane you would like to get and it returns it
+     * Returns null if out of bounds
+     */
+    /*public GameObject getLane(int num)
+    {
+        try
         {
-            if (currentLanes == 0) lane.transform.Translate(3 * distance * Vector2.left);
-            currentLanes++;
-            lane.transform.Translate(distance * Vector2.right);
-            if (currentLanes < numLanes)
-            {
-                Instantiate(lane);
-            }
+            return lanes[num];
+        }
+        catch (Exception e)
+        {
+            debu
+            return null;
         }
     }
+    /**
+     * getLaneLeft
+     * Input number of lane you would like to get and it returns the lane to the left
+     * Returns null if out of bounds
+     */
+    /*public GameObject getLaneLeft(int num)
+    {
+        try
+        {
+            return lanes[num-1];
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+    }
+    /**
+     * getLaneRight
+     * Input number of lane you would like to get and it returns the lane to the left
+     * Returns null if out of bounds
+     */
+    /*public GameObject getLaneRightt(int num)
+    {
+        try
+        {
+            return lanes[num+1];
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+    }//*/
 }
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -10,22 +10,20 @@ public class PlayerControl : MonoBehaviour
     [SerializeField] private Transform[] lanes;
     [SerializeField] private int currentLane;
     [SerializeField] private float speed;
+    [SerializeField] private float speedCurve; //Needs to be >0 and <2
+    [SerializeField] private float yPos;
+    private float interpolator;
+    private Vector3 oldPosition;
+    private float posDiff;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //Make a list of lanes
-        Transform[] temp = laneGroupObject.GetComponentsInChildren<Transform>();
-        lanes = new Transform[5];
-        //If someone can think of a smarter way to filter out the player spot transforms than hard coding, be my guest
-        for (int i = 1; i <= 5; i++)
-        {
-            this.lanes[i - 1] = temp[i * 3];
-        }//*/
-
         //Start player in the center lane
-        transform.position = lanes[2].position;
+        transform.SetPositionAndRotation(new Vector3(lanes[2].position.x+0.01f, yPos, 0), new Quaternion());
+        oldPosition = transform.position;
         currentLane = 2;
+        interpolator = 0.0f;
     }
 
     // Update is called once per frame
@@ -35,19 +33,38 @@ public class PlayerControl : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
             if (currentLane > 0) currentLane--;
+            interpolator = 0;
+            oldPosition = transform.position;
+            posDiff = Mathf.Abs(oldPosition.x - lanes[currentLane].transform.position.x);
         }
         if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
             if (currentLane < 4) currentLane++;
+            interpolator = 0;
+            oldPosition = transform.position;
+            posDiff = Mathf.Abs(oldPosition.x - lanes[currentLane].transform.position.x);
         }
-        //Move the player toward the target lane
-        //transform.position = lanes[currentLane].position;
-        if (lanes[currentLane].position.x < transform.position.x) {
+
+        //Old Movement code, will delete after lerp is confirmed to work
+        /*if (lanes[currentLane].position.x < transform.position.x) {
             transform.Translate(-speed*Time.deltaTime,0,0);
         }
         if (lanes[currentLane].position.x > transform.position.x) {
             transform.Translate(speed*Time.deltaTime,0,0);
         }//*/
+
+        //Move the player based on the lerp between the positions and the interpolator's current value
+        transform.position = new Vector3(Mathf.Lerp(oldPosition.x, lanes[currentLane].transform.position.x, interpolator), yPos, 0);
+
+        //TLDR: These lines makes the car go slower at either extreme of its motion
+        //This makes the interpolator move like a Bell Curve, we calculate the percentage of the distance travelled thus far
+        //We then subtract 50% from it to get a range of -0.5 to 0.5, where either extreme occurs when close to either extreme of the position
+        //We then take the absolute value of that and subtract it from 1 to make sure that in the middle the car is moving at the highest speed 
+        float currentDiff = Mathf.Abs(transform.position.x - lanes[currentLane].transform.position.x);
+        float diffPercent = 1 - speedCurve * Mathf.Abs(currentDiff/posDiff - 0.5f);
+
+        //Adjust the interpolator
+        interpolator += diffPercent * speed * Time.deltaTime;
     }
 
 }
