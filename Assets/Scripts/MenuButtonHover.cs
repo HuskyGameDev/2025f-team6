aa3a361061fbfbc7f823a061181ba6f620474d17
Technophobia <dmwheele@mtu.edu>
2026-01-23 00:12:42 -0500

    Updated Menu Buttons
    Updates the Michigan State Police logo on the main menu to pixel art (still changable)
    
    Updates play, options, about, quit buttons to blocky pixel art text rendered from 3d

--- Assets/Scripts/MenuButtonHover.cs
+++ Assets/Scripts/MenuButtonHover.cs
@@ -1,22 +1,30 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System.Drawing;
 
 public class MenuButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public MainMenuButtonsController menuController;
     public RawImage icon;
 
+    public Texture2D BaseSprite;
+    public Texture2D HoverSprite;
+
+
     Button myButton;
 
     void Awake()
     {
+        WarnNull();
         myButton = GetComponent<Button>();
+
+
         //if (icon.name != "RawImageA")
         //{
         //    icon.enabled = false;
         //}
-        if (icon) icon.enabled = false;
+        //if (icon) icon.enabled = false;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -25,7 +33,7 @@ public class MenuButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExit
         {
             if (icon != null)
             {
-                icon.enabled = true;
+                icon.texture = HoverSprite;
             }
 
             menuController.OnButtonHover(myButton);
@@ -38,9 +46,17 @@ public class MenuButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExit
         {
             if (icon != null)
             {
-                icon.enabled = false;
+                icon.texture = BaseSprite;
             }
+            
             menuController.OnButtonExit(myButton);
         }
+    }
+
+    public void WarnNull()
+    {
+        if (icon == null) Debug.LogWarning("Icon RawImage not assigned in MenuButtonHover on " + gameObject.name);
+        if (BaseSprite == null) Debug.LogWarning("BaseSprite not assigned in MenuButtonHover on " + gameObject.name);
+        if (HoverSprite == null) Debug.LogWarning("HoverSprite not assigned in MenuButtonHover on " + gameObject.name);
     }
 }
