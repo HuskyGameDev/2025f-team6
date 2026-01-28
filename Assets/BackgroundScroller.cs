066b63d2d9e27966d1b631548f3ba1e0716c4676
dmwheeler <dmwheele@mtu.edu>
2026-01-28 02:35:54 -0500

    Revert "Background Overhaul"

--- Assets/BackgroundScroller.cs
+++ Assets/BackgroundScroller.cs
@@ -2,25 +2,14 @@ using UnityEngine;
 
 public class BackgroundScroller : MonoBehaviour
 {
-    [SerializeField]
-    private bool isScrolling = true;
-
-    [SerializeField]
-    public float scrollSpeed = 1;
-
-    [SerializeField]
-    private new Camera camera;
+    [SerializeField] private bool isScrolling = true;
+    [SerializeField] public float scrollSpeed = 1;
+    [SerializeField] private new Camera camera;
     private Vector3 startPos;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         startPos = this.transform.position;
-
-        if (ScrollSpeedProvider.Instance != null)
-        {
-            ScrollSpeedProvider.Instance.SetBaseScrollSpeed(scrollSpeed);
-        }
     }
 
     // Update is called once per frame
@@ -28,14 +17,7 @@ public class BackgroundScroller : MonoBehaviour
     {
         if (isScrolling)
         {
-            float speed = scrollSpeed;
-
-            if (ScrollSpeedProvider.Instance != null)
-            {
-                speed = ScrollSpeedProvider.Instance.CurrentSpeed;
-            }
-
-            transform.position -= new Vector3(0, speed * Time.deltaTime, 0);
+            transform.position -= new Vector3(0, scrollSpeed * Time.deltaTime, 0);
             if (this.transform.position.y <= camera.transform.position.y - camera.orthographicSize)
             {
                 this.transform.position = startPos;
@@ -56,10 +38,5 @@ public class BackgroundScroller : MonoBehaviour
     void setScrollSpeed(float speed)
     {
         scrollSpeed = speed;
-
-        if (ScrollSpeedProvider.Instance != null)
-        {
-            ScrollSpeedProvider.Instance.SetBaseScrollSpeed(scrollSpeed);
-        }
     }
 }
--- Assets/Scripts/ObstacleController.cs
+++ Assets/Scripts/ObstacleController.cs
@@ -3,36 +3,21 @@ using UnityEngine;
 public class ObstacleController : MonoBehaviour
 {
     [Header("Movement Settings")]
-    [SerializeField]
-    private float moveSpeed = 5f;
-
-    [SerializeField]
-    private bool useRandomSpeed = false;
-
-    [SerializeField]
-    private float minSpeed = 3f;
-
-    [SerializeField]
-    private float maxSpeed = 8f;
-
-    [SerializeField]
-    private float verticalSpeedMultiplier = 1f;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private bool useRandomSpeed = false;
+    [SerializeField] private float minSpeed = 3f;
+    [SerializeField] private float maxSpeed = 8f;
 
     [Header("Destruction Settings")]
-    [SerializeField]
-    private float destroyYPosition = -10f;
+    [SerializeField] private float destroyYPosition = -10f;
+    [SerializeField] private bool destroyWhenInvisible = true;
 
-    [SerializeField]
-    private bool destroyWhenInvisible = true;
 
     [Header("Sound Effects")]
-    [SerializeField]
-    private AudioClip hitSoundClip;
+    [SerializeField] private AudioClip hitSoundClip;
 
     private Camera mainCamera;
-
-    [SerializeField]
-    private float speed;
+    [SerializeField] private float speed;
     private ObstacleSpawner spawner;
     private GameObject originalPrefab;
     public bool isActive = false;
@@ -43,17 +28,10 @@ public class ObstacleController : MonoBehaviour
 
     void Update()
     {
-        if (!gameObject.activeSelf)
-            return;
+        if (!gameObject.activeSelf) return;
 
-        float scrollSpeed = speed;
-
-        if (ScrollSpeedProvider.Instance != null)
-        {
-            scrollSpeed = ScrollSpeedProvider.Instance.CurrentSpeed;
-        }
-
-        transform.Translate(Vector3.down * scrollSpeed * verticalSpeedMultiplier * Time.deltaTime);
+        // Move obstacle downward with speed multiplier
+        transform.Translate(Vector3.down * speed * speedMultiplier * Time.deltaTime);
 
         // Check if off screen
         if (IsOffScreen())
@@ -77,25 +55,17 @@ public class ObstacleController : MonoBehaviour
     {
         mainCamera = Camera.main;
 
-        float scrollSpeed = moveSpeed;
-
-        if (ScrollSpeedProvider.Instance != null)
+        // Set movement speed
+        if (useRandomSpeed)
         {
-            scrollSpeed = ScrollSpeedProvider.Instance.CurrentSpeed;
+            speed = Random.Range(minSpeed, maxSpeed);
+        }
+        else
+        {
+            speed = moveSpeed;
         }
 
-        speed = scrollSpeed;
-
-        if (useRandomSpeed && ScrollSpeedProvider.Instance != null)
-        {
-            float baseScroll = ScrollSpeedProvider.Instance.GetBaseScrollSpeed();
-            if (baseScroll > 0.0001f)
-            {
-                float randomAbsolute = Random.Range(minSpeed, maxSpeed);
-                verticalSpeedMultiplier = randomAbsolute / baseScroll;
-            }
-        }
-
+        // Store base speed for multiplier calculations
         baseSpeed = speed;
 
         // Get current speed multiplier from spawner if available
@@ -157,15 +127,15 @@ public class ObstacleController : MonoBehaviour
     }
 
     private void HandlePlayerCollision(GameObject player)
-    {
+    {       
         // Trigger collision effects on player
         PlayerCollision playerCollision = player.GetComponent<PlayerCollision>();
-
+        
         if (playerCollision != null)
         {
             // Play SFX
             AudioManager.instance.PlaySoundFXClip(hitSoundClip, transform, 1f);
-
+            
             // You might want to pass this obstacle to the player collision handler
             playerCollision.HandleCollision(gameObject);
         }
@@ -189,11 +159,7 @@ public class ObstacleController : MonoBehaviour
         if (!destroyWhenInvisible)
         {
             Gizmos.color = Color.yellow;
-            Vector3 destructionPoint = new Vector3(
-                transform.position.x,
-                destroyYPosition,
-                transform.position.z
-            );
+            Vector3 destructionPoint = new Vector3(transform.position.x, destroyYPosition, transform.position.z);
             Gizmos.DrawLine(transform.position, destructionPoint);
             Gizmos.DrawWireCube(destructionPoint, Vector3.one);
         }
