47131c5ce733c0f4c2cb1f27fe404a63f39ca1c3
Tyler Gingras <tsgingra@mtu.edu>
2026-03-06 14:56:51 -0500

    Criminal lanes, main menu background
    - Fixed criminal lane appearance so it can appear in any lane randomly. Disables obstacle spawning in that lane.
    - Background scroller for main menu.
    - Added some new sounds.

--- Assets/Animations/PlayAnimation.cs
+++ Assets/Animations/PlayAnimation.cs
@@ -5,31 +5,50 @@ using System.Collections;
 public class PlayTimelineRegularly : MonoBehaviour
 {
     private PlayableDirector director;
+    public LaneEnable laneEnable;
     public GameObject criminal;
     public GameObject[] lanes;
-    public float interval = 60f;
-    public float offset = 15f;
+    public float interval = 15f;
+    public float offset = 15f;   
     public bool play = true;
 
     void Start()
     {
         director = GetComponent<PlayableDirector>();
-        StartCoroutine(PlayTimelineRoutine(offset));
+        if (director != null)
+        {
+            StartCoroutine(PlayTimelineRoutine(offset));
+        }
     }
 
-    IEnumerator PlayTimelineRoutine(float offset)
+    IEnumerator PlayTimelineRoutine(float initialDelay)
     {
-        yield return new WaitForSeconds(offset);
+        yield return new WaitForSeconds(initialDelay);
 
         while (play)
         {
-            if (director != null)
-            {
-                // Randomize lane
-                //criminal.transform.parent = lanes[Random.Range(0, 4)];
-                director.Play(); // Plays the timeline from the beginning
-            }
+            // Randomize Lane
+            int laneIndex = Random.Range(0, lanes.Length);
+
+            // Set randomized lane parent and local position of criminal.
+            criminal.transform.SetParent(lanes[laneIndex].transform);
+            criminal.transform.localPosition = Vector3.zero;
+            criminal.transform.localRotation = Quaternion.identity;
+
+            // Disable obstacle spawning in that lane.
+            laneEnable.DisableLane(laneIndex);
+
+            // Play animation and wait for it to end.
+            director.Play();
+            yield return new WaitForSeconds((float) director.duration);
+
+            // Enable obstacle spawning in that lane.
+            laneEnable.EnableLanes();
+
+            // Wait for designated interval between animations.
             yield return new WaitForSeconds(interval);
+
         }
+        
     }
 }
